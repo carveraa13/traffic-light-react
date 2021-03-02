774f60659780c2ceb5da48f9095443c91132f71a
Carlos Vera A <carveraa13@gmail.com>
2021-03-02 18:25:23 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+// import { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [colorSelect, colfun] = useState("");
+	const redSelected = colorSelect == "red" ? "selected" : "";
+	const yellowSelected = colorSelect == "yellow" ? "selected" : "";
+	const greenSelected = colorSelect == "green" ? "selected" : "";
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center m-5">
+			<div id="trafficBody">
+				<div
+					className={"light red " + redSelected}
+					onClick={() => colfun("red")}></div>
+				<div
+					className={"light yellow " + yellowSelected}
+					onClick={() => colfun("yellow")}></div>
+				<div
+					className={"light green " + greenSelected}
+					onClick={() => colfun("green")}></div>
+			</div>
 		</div>
 	);
 }
