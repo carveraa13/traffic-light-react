c81a6da26052b7a3a31ef3e41f781049c440728f
Carlos Vera A <carveraa13@gmail.com>
2021-03-03 05:00:07 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,9 +1,5 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-// import { useState } from "react";
-
 //create your first component
 export function Home() {
 	const [colorSelect, colfun] = useState("");
@@ -12,7 +8,8 @@ export function Home() {
 	const greenSelected = colorSelect == "green" ? "selected" : "";
 
 	return (
-		<div className="text-center m-5">
+		<div>
+			<div id="trafficTop"></div>
 			<div id="trafficBody">
 				<div
 					className={"light red " + redSelected}
